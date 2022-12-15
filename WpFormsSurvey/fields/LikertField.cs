fdba26f955f9573a7096ce25b46356950e36e266
Mark Olbert <mark@arcabama.com>
2022-12-15 09:38:45 -0800

    implement row/column initialization

--- WpFormsSurvey/fields/LikertField.cs
+++ WpFormsSurvey/fields/LikertField.cs
@@ -1,11 +1,16 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
+using WpFormsSurvey;
 
 namespace WPFormsSurvey;
 
 [JsonFieldName("likert-scale")]
 public class LikertField : TextField
 {
+    [JsonPropertyName("single_row")]
+    [JsonConverter(typeof(WpFormsBooleanConverter))]
+    public bool SingleRow { get; set; }
+
     [JsonPropertyName("rows")]
     public JsonElement RawRows { get; set; }
 
@@ -17,4 +22,28 @@ public class LikertField : TextField
 
     [JsonIgnore]
     public List<string> Columns { get; } = new();
+
+    public override bool Initialize()
+    {
+        if( !base.Initialize() )
+            return false;
+
+        // WpForms stores rows and columns as JsonObjects with each property corresponding
+        // to a row or column. However, the property names are not valid under C#
+        // (they're the row or column index)
+        Rows.Clear();
+        Columns.Clear();
+
+        foreach (var row in RawRows.EnumerateObject())
+        {
+            Rows.Add(row.Value.GetString() ?? string.Empty);
+        }
+
+        foreach (var column in RawColumns.EnumerateObject())
+        {
+            Columns.Add(column.Value.GetString() ?? string.Empty);
+        }
+
+        return true;
+    }
 }
