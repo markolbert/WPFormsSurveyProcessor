edb8c06695e615b641698927108a8a6408732f77
Mark Olbert <mark@arcabama.com>
2022-12-15 10:01:25 -0800

    test for valid choice and likert fields

--- Test.Parsing/ParseFormDefinition.cs
+++ Test.Parsing/ParseFormDefinition.cs
@@ -1,6 +1,7 @@
 using System.Dynamic;
 using System.Text.Json;
 using FluentAssertions;
+using FluentAssertions.Equivalency.Steps;
 using WpFormsSurvey;
 using WPFormsSurvey;
 
@@ -16,6 +17,42 @@ public class ParseFormDefinition : TestBase
         var result = parser.ParseFile( filePath );
 
         result.Should().NotBeNull();
-        result!.IsValid.Should().BeTrue();
+        result!.Table.Should().NotBeNull();
+        result.Table!.Data.Should().NotBeNull();
+        result.IsValid.Should().BeTrue();
+
+        foreach( var choiceField in result.Table.Data!.SelectMany( x => x.Fields )
+                                          .Where( x => x.GetType().IsAssignableTo( typeof( ChoicesField ) ) )
+                                          .Cast<ChoicesField>() )
+        {
+            foreach( var choice in choiceField.Choices )
+            {
+                choice.Label.Should().NotBeNullOrEmpty();
+            }
+        }
+
+        foreach (var likertField in result.Table.Data!.SelectMany(x => x.Fields)
+                                          .Where(x => x.GetType().IsAssignableTo(typeof(LikertField)))
+                                          .Cast<LikertField>())
+        {
+            if( likertField.SingleRow )
+                likertField.Rows.Should().BeEmpty();
+            else
+            {
+                likertField.Rows.Should().NotBeEmpty();
+
+                foreach( var row in likertField.Rows )
+                {
+                    row.Should().NotBeNullOrEmpty();
+                }
+            }
+
+            likertField.Columns.Should().NotBeEmpty();
+            
+            foreach (var column in likertField.Columns)
+            {
+                column.Should().NotBeNullOrEmpty();
+            }
+        }
     }
 }
