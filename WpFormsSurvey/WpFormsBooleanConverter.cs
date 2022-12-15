3c8cc4a573c91f038ff6f6586557c8c6ea180f2c
Mark Olbert <mark@arcabama.com>
2022-12-15 09:39:03 -0800

    add WpForms converter for text/numeric booleans

---
+++ WpFormsSurvey/WpFormsBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WpFormsSurvey;
+
+public class WpFormsBooleanConverter : JsonConverter<bool>
+{
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var text = reader.GetString();
+
+        if (int.TryParse(text, out var temp))
+            return temp == 1;
+
+        return false;
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value ? "1" : "0");
+    }
+}
