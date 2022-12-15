639c5029b9a22c5c51ed6a900cc7dd7ac55ba9e7
Mark Olbert <mark@arcabama.com>
2022-12-14 20:39:24 -0800

    add DateTime parser/converter

---
+++ WpFormsSurvey/DateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WpFormsSurvey;
+public class DateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+    {
+        var text = reader.GetString();
+
+        return DateTime.TryParse(text, out var dtValue) ? dtValue : DateTime.MinValue;
+    }
+
+    public override void Write( Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options )
+    {
+        writer.WriteStringValue( value );
+    }
+}
