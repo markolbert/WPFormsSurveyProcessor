b52c24eb599901ff260fc63d30dd9efee2e55a94
Mark Olbert <mark@arcabama.com>
2022-12-15 12:30:22 -0800

    add converter for WpForms integer fields

---
+++ WpFormsSurvey/WpFormsIntegerConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WpFormsSurvey;
+
+public class WpFormsIntegerConverter : JsonConverter<int>
+{
+    public override int Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options ) =>
+        int.TryParse( reader.GetString(), out var temp ) ? temp : 0;
+
+    public override void Write( Utf8JsonWriter writer, int value, JsonSerializerOptions options ) =>
+        writer.WriteStringValue( value.ToString() );
+}
