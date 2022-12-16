75d875891f7e10a95cbb469c76b33740ecc8ce3f
Mark Olbert <mark@arcabama.com>
2022-12-15 19:30:24 -0800

    add summary properties

--- WpFormsSurvey/download/forms/DownloadFormTable.cs
+++ WpFormsSurvey/download/forms/DownloadFormTable.cs
@@ -1,3 +1,11 @@
 ﻿namespace WpFormsSurvey;
 
-public record DownloadFormTable( string Name, string Database, List<FormDefinition>? Data );
+public record DownloadFormTable( string Name, string Database, List<FormDefinition>? Data )
+{
+    public List<FormInfo> Forms =>
+        Data?.Select( x => new FormInfo( x.Id, x.PostTitle ) )
+             .Distinct( FormInfo.DefaultComparer )
+             .OrderBy( x => x.Id )
+             .ToList()
+     ?? new List<FormInfo>();
+}
--- WpFormsSurvey/download/responses/DownloadResponseTable.cs
+++ WpFormsSurvey/download/responses/DownloadResponseTable.cs
@@ -1,3 +1,11 @@
 ﻿namespace WpFormsSurvey;
 
-public record DownloadResponseTable(string Name, string Database, List<ResponseDefinition>? Data);
+public record DownloadResponseTable( string Name, string Database, List<ResponseDefinition>? Data )
+{
+    public List<int> FormIds =>
+        Data?.Select( x => x.FormId )
+             .Distinct()
+             .OrderBy( x => x )
+             .ToList()
+     ?? new List<int>();
+}
