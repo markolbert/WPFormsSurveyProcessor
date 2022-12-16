e841cd4c290d14486254d82f872646409f23dc72
Mark Olbert <mark@arcabama.com>
2022-12-15 19:37:45 -0800

    add Likert response

---
+++ WpFormsSurvey/responses/LikertResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WpFormsSurvey;
+
+[WpFormsFieldType("likert-scale")]
+public class LikertResponse : ResponseBase
+{
+    [JsonPropertyName("value_raw")]
+    public string? RawValue { get; set; }
+
+    [JsonIgnore]
+    public List<LikertScore> Scores { get; } = new();
+}
---
+++ WpFormsSurvey/responses/LikertScore.cs
@@ -0,0 +1,3 @@
+﻿namespace WpFormsSurvey;
+
+public record LikertScore( int Row, int Column );
