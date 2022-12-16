26e74e8e6d36df1890340aea2a60c2c85fbe4352
Mark Olbert <mark@arcabama.com>
2022-12-15 19:35:37 -0800

    add IEqualityComparer

---
+++ WpFormsSurvey/download/responses/FormInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace WpFormsSurvey;
+
+public record FormInfo( int Id, string Name )
+{
+    private sealed class IdEqualityComparer : IEqualityComparer<FormInfo>
+    {
+        public bool Equals( FormInfo? x, FormInfo? y )
+        {
+            if( ReferenceEquals( x, y ) )
+                return true;
+            if( ReferenceEquals( x, null ) )
+                return false;
+            if( ReferenceEquals( y, null ) )
+                return false;
+            if( x.GetType() != y.GetType() )
+                return false;
+
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode( FormInfo obj )
+        {
+            return obj.Id;
+        }
+    }
+
+    public static IEqualityComparer<FormInfo> DefaultComparer { get; } = new IdEqualityComparer();
+}
