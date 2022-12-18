54636a04cdd9a9192a0e92c3cb4d70858de743a5
Mark Olbert <mark@arcabama.com>
2022-12-17 20:42:32 -0800

    implemented form info service

--- WPFormsSurveyProcessor/services/FormInfoService.cs
+++ WPFormsSurveyProcessor/services/FormInfoService.cs
@@ -1,4 +1,8 @@
-﻿using J4JSoftware.Logging;
+﻿using Alba.CsConsoleFormat;
+using J4JSoftware.DeusEx;
+using J4JSoftware.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using WpFormsSurvey;
 
 namespace WPFormsSurveyProcessor;
 
@@ -10,5 +14,75 @@ internal class FormInfoService : ServiceBase
     )
         : base( config, logger )
     {
+    }
+
+    public override Task StartAsync( CancellationToken cancellationToken )
+    {
+        if( Configuration.PostsFileStatus == Configuration.FileStatus.Okay )
+            ParseFormDefinitions();
+        else ParseFormResponses();
+
+        return Task.CompletedTask;
+    }
+
+    private void ParseFormDefinitions()
+    {
+        var parser = J4JDeusEx.ServiceProvider.GetRequiredService<WpFormsParser>();
+        var forms = parser.ParseFile( Configuration.PostsFilePath );
+
+        if( forms?.Table == null )
+        {
+            Logger.Error( "Failed to parse WordPress posts file" );
+            return;
+        }
+
+        var headerThickness = new LineThickness(LineWidth.Single, LineWidth.Single);
+
+        var doc = new Document( new Span( "WpForms Form Info" ) { Color = ConsoleColor.Magenta },
+                                new Grid
+                                {
+                                    Color = ConsoleColor.Gray,
+                                    Columns = { GridLength.Auto, GridLength.Auto },
+                                    Children =
+                                    {
+                                        new Cell( "Id" ) { Stroke = headerThickness },
+                                        new Cell( "Title" ) { Stroke = headerThickness },
+                                        forms!.Table!.SummaryInfo.Select( x => new[]
+                                        {
+                                            new Cell( x.Id ), new Cell( x.Name )
+                                        } )
+                                    }
+                                } );
+
+        ConsoleRenderer.RenderDocument( doc );
+    }
+
+    private void ParseFormResponses()
+    {
+        var parser = J4JDeusEx.ServiceProvider.GetRequiredService<WpResponsesParser>();
+        var responses = parser.ParseFile(Configuration.EntriesFilePath);
+
+        if (responses?.Table == null)
+            Logger.Error("Failed to parse WordPress posts file");
+
+        var headerThickness = new LineThickness(LineWidth.Single, LineWidth.Single);
+
+        var doc = new Document( new Span( "WpForms Form Info" ) { Color = ConsoleColor.Magenta },
+                                new Grid
+                                {
+                                    Color = ConsoleColor.Gray,
+                                    Columns = { GridLength.Auto, GridLength.Auto },
+                                    Children =
+                                    {
+                                        new Cell( "Id" ) { Stroke = headerThickness },
+                                        new Cell( "Title" ) { Stroke = headerThickness },
+                                        responses!.Table!.FormIds.Select( x => new[]
+                                        {
+                                            new Cell( x )
+                                        } )
+                                    }
+                                } );
+
+        ConsoleRenderer.RenderDocument( doc );
     }
 }
