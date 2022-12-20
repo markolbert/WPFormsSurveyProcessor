d734dd38bfe1c66c5baa93ec6311df59d5a82fbf
Mark Olbert <mark@arcabama.com>
2022-12-19 20:10:48 -0800

    remove unneeded code

--- WpFormsSurvey/download/forms/Forms.cs
+++ WpFormsSurvey/download/forms/Forms.cs
@@ -1,11 +1,3 @@
 ﻿namespace WpFormsSurvey;
 
-public record Forms( string Name, string Database, List<Form>? Data )
-{
-    public List<FormInfo> SummaryInfo =>
-        Data?.Select( x => new FormInfo( x.Id, x.PostTitle ) )
-             .Distinct( FormInfo.DefaultComparer )
-             .OrderBy( x => x.Id )
-             .ToList()
-     ?? new List<FormInfo>();
-}
+public record Forms( string Name, string Database, List<Form>? Data );
