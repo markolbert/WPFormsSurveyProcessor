33ee5a7b0694729be157e5559f4d6768d557f6cd
Mark Olbert <mark@arcabama.com>
2022-12-19 20:17:34 -0800

    expand applicability to more field types

--- WpFormsSurvey/responses/TextResponse.cs
+++ WpFormsSurvey/responses/TextResponse.cs
@@ -3,8 +3,11 @@
 [WpFormsFieldType("text")]
 [WpFormsFieldType("textarea")]
 [WpFormsFieldType("radio")]
+[WpFormsFieldType("select")]
 [WpFormsFieldType("phone")]
 [WpFormsFieldType("email")]
+[WpFormsFieldType("password")]
+[WpFormsFieldType("file-upload")]
 public class TextResponse : ResponseBase
 {
     public string Value { get; set; } = string.Empty;
