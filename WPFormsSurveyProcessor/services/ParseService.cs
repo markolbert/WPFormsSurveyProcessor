290ade11fa497099998273812622c7b1f4afc47d
Mark Olbert <mark@arcabama.com>
2022-12-26 11:40:25 -0800

    move export file validation to configuration object

--- WPFormsSurveyProcessor/services/ParseService.cs
+++ WPFormsSurveyProcessor/services/ParseService.cs
@@ -1,6 +1,5 @@
 ﻿using J4JSoftware.Logging;
 using NPOI.XSSF.UserModel;
-using Org.BouncyCastle.Math.EC;
 using WpFormsSurvey;
 
 namespace WPFormsSurveyProcessor;
@@ -33,73 +32,16 @@ internal class ParseService : ServiceBase
         _choiceFieldsExporter = choiceFieldsExporter;
         _fieldDescriptionsExporter = fieldDescriptionsExporter;
         _submissionsExporter = submissionsExporter;
-
-        ValidateExcelFilePath();
-    }
-
-    private void ValidateExcelFilePath()
-    {
-        var excelDir = Path.GetDirectoryName( Configuration.ExcelFilePath );
-        if( !string.IsNullOrEmpty(excelDir  ) && excelDir.Intersect(Path.GetInvalidPathChars()).Any())
-        {
-            Logger.Warning<string>(
-                "Excel output directory '{0}' contains invalid characters, changing output file to ExcelOutput.xlsx in the current directory",
-                excelDir );
-
-            Configuration.ExcelFilePath = Path.Combine( Environment.CurrentDirectory, "ExcelOutput.xlsx" );
-            return;
-        }
-
-        var excelFile = Path.GetFileNameWithoutExtension(Configuration.ExcelFilePath);
-        if (!string.IsNullOrEmpty(excelFile) && excelFile.Intersect(Path.GetInvalidFileNameChars()).Any())
-        {
-            Logger.Warning<string>(
-                "Excel output file name '{0}' contains invalid characters, changing output file to ExcelOutput.xlsx in the current directory",
-                excelFile);
-
-            Configuration.ExcelFilePath = Path.Combine(Environment.CurrentDirectory, "ExcelOutput.xlsx");
-            return;
-        }
-
-        var extension = Path.GetExtension( Configuration.ExcelFilePath ).ToLower();
-        if( extension != ".xlsx" )
-        {
-            Logger.Warning<string>("Unsupported or invalid file extension '{0}' changed to .xlsx", extension  );
-            Configuration.ExcelFilePath = $"{Path.GetFileNameWithoutExtension(Configuration.ExcelFilePath)}{extension}";
-        }
-
-        //see if the file can be created where specified
-        var fileDir = Path.GetDirectoryName( Configuration.ExcelFilePath ) ?? Environment.CurrentDirectory;
-        var testFile = Path.Combine( fileDir, Guid.NewGuid().ToString() );
-
-        var canCreate = false;
-
-        try
-        {
-            using var fs = File.Create( testFile );
-
-            fs.Close();
-            File.Delete( testFile );
-            
-            canCreate = true;
-        }
-        // ReSharper disable once EmptyGeneralCatchClause
-        catch
-        {
-        }
-
-        if( canCreate )
-            return;
-
-        Logger.Warning<string>(
-            "{0} is inaccessible, changing output file to ExcelOutput.xlsx in the current directory",
-            Configuration.ExcelFilePath );
-
-        Configuration.ExcelFilePath = Path.Combine( Environment.CurrentDirectory, "ExcelOutput.xlsx" );
     }
 
     public override Task StartAsync( CancellationToken cancellationToken )
     {
+        if( !Configuration.ExcelFileInfo.CanBeWritten )
+        {
+            Logger.Error("Cannot create or write to the Excel file path");
+            return Task.CompletedTask;
+        }
+
         var formsDownload = ParseForms();
         if( formsDownload?.Data is not {} formDefinitions )
             return Task.CompletedTask;
@@ -136,25 +78,27 @@ internal class ParseService : ServiceBase
 
         var workbook = new XSSFWorkbook( XSSFWorkbookType.XLSX );
 
-        if( ( Configuration.InformationToExport & Exporters.FormInformation ) == Exporters.FormInformation
+        if( ( Configuration.ExcelFileInfo.InformationToExport & Exporters.FormInformation ) == Exporters.FormInformation
         && !ExportFormInfo( workbook, formDefinitions ) )
             return Task.CompletedTask;
 
-        if( ( Configuration.InformationToExport & Exporters.FieldDescriptions ) == Exporters.FieldDescriptions
+        if( ( Configuration.ExcelFileInfo.InformationToExport & Exporters.FieldDescriptions ) == Exporters.FieldDescriptions
         && !ExportFieldDescriptions( workbook, formDefinitions ) )
             return Task.CompletedTask;
 
-        if( ( Configuration.InformationToExport & Exporters.ChoiceFields ) == Exporters.ChoiceFields
+        if( ( Configuration.ExcelFileInfo.InformationToExport & Exporters.ChoiceFields ) == Exporters.ChoiceFields
         && !ExportChoiceFields( workbook, formDefinitions ) )
             return Task.CompletedTask;
 
-        if( ( Configuration.InformationToExport & Exporters.Submissions ) == Exporters.Submissions
+        if( ( Configuration.ExcelFileInfo.InformationToExport & Exporters.Submissions ) == Exporters.Submissions
         && !ExportSubmissions( workbook, new SubmissionInfo( formDefinitions, individualSubmissions ) ) )
             return Task.CompletedTask;
 
         try
         {
-            using var fileStream = File.Create( Configuration.ExcelFilePath );
+            using var fileStream = File.Create( Configuration.ExcelFileInfo
+                                                             .GetTimeStampedPath( Configuration.EntriesFilePath ) );
+
             Logger.Information<string>( "Writing results to {0}", fileStream.Name );
 
             workbook.Write( fileStream );
