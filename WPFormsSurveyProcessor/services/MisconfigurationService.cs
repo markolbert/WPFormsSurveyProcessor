aa1bcf3cd825826c68e2d3e1cae99f0f4a16b26b
Mark Olbert <mark@arcabama.com>
2022-12-27 15:59:57 -0800

    display help before errors

--- WPFormsSurveyProcessor/services/MisconfigurationService.cs
+++ WPFormsSurveyProcessor/services/MisconfigurationService.cs
@@ -1,21 +1,30 @@
-﻿using J4JSoftware.Logging;
+﻿using J4JSoftware.Configuration.CommandLine;
+using J4JSoftware.Configuration.J4JCommandLine;
+using J4JSoftware.Logging;
 using Serilog.Events;
 
 namespace WPFormsSurveyProcessor;
 
 internal class MisconfigurationService : ServiceBase
 {
+    private readonly OptionCollection _options;
+
     public MisconfigurationService(
         Configuration config,
+        OptionCollection options,
         IJ4JLogger logger
     )
         : base(config, logger)
     {
+        _options = options;
     }
 
     public override Task StartAsync( CancellationToken cancellationToken )
     {
-        if( Configuration.Errors == null )
+        var help = new ColorHelpDisplay(new WindowsLexicalElements(Logger), _options);
+        help.Display();
+
+        if ( Configuration.Errors == null )
             return Task.CompletedTask;
 
         foreach (var error in Configuration.Errors)
