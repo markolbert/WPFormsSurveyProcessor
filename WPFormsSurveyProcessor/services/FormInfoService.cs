b104f51d8a8cb9ba79a9e723196c0ae61fbbe014
Mark Olbert <mark@arcabama.com>
2022-12-27 20:16:13 -0800

    added copyright/license headers

--- WPFormsSurveyProcessor/services/FormInfoService.cs
+++ WPFormsSurveyProcessor/services/FormInfoService.cs
@@ -1,4 +1,21 @@
-﻿using Alba.CsConsoleFormat;
+﻿// Copyright (c) 2022 Mark A. Olbert 
+// all rights reserved
+// This file is part of WpFormsSurveyProcessor.
+//
+// WpFormsSurveyProcessor is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// WpFormsSurveyProcessor is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with WpFormsSurveyProcessor. If not, see <https://www.gnu.org/licenses/>.
+
+using Alba.CsConsoleFormat;
 using J4JSoftware.DeusEx;
 using J4JSoftware.Logging;
 using Microsoft.Extensions.DependencyInjection;
--- WPFormsSurveyProcessor/services/MisconfigurationService.cs
+++ WPFormsSurveyProcessor/services/MisconfigurationService.cs
@@ -1,4 +1,21 @@
-﻿using J4JSoftware.Configuration.CommandLine;
+﻿// Copyright (c) 2022 Mark A. Olbert 
+// all rights reserved
+// This file is part of WpFormsSurveyProcessor.
+//
+// WpFormsSurveyProcessor is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// WpFormsSurveyProcessor is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with WpFormsSurveyProcessor. If not, see <https://www.gnu.org/licenses/>.
+
+using J4JSoftware.Configuration.CommandLine;
 using J4JSoftware.Configuration.J4JCommandLine;
 using J4JSoftware.Logging;
 using Serilog.Events;
